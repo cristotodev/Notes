3834348f0fbe522ee6292a64ef4569a7197e53f0
Cristo Estévez <cristom.estevez@gmail.com>
2018-11-23 14:27:25 +0000

    Fixed problems with user login on passport.js and solved links for views.

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,30 +1,30 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const mongoose = require('mongoose');
 const UserModel = require('../models/User');
 
 passport.use(new LocalStrategy({
     usernameField: 'email'
-}), async(email, password, done) => {
-    const userModel = await UserModel.findOne({ email: email });
-    if (!userModel) {
-        return done(null, false, { message: 'Not user found.' });
+}, async(email, password, done) => {
+    // Match Email's User
+    const user = await UserModel.findOne({ email: email });
+    if (!user) {
+        return done(null, false, { message: 'Not User found.' });
     }
-
-    const match = await userModel.matchPassword(password);
+    const match = await user.matchPassword(password);
     if (match) {
         return done(null, user);
     }
-
-    return done(done, null, { message: 'Incorrect password' });
-});
+    return done(null, false, { message: 'Incorrect Password.' });
+}));
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser(async(id, done) => {
-    await UserModel.findById(id, (err, user) => {
+passport.deserializeUser((id, done) => {
+    UserModel.findById(id, (err, user) => {
         done(err, user);
     });
 });
