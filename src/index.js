276b7da4d6e9d2bd54d654773ac82e468204293c
Cristo Estévez <cristom.estevez@gmail.com>
2018-11-23 12:48:34 +0000

    Added url to delete a specify note, and added alert message successfully on all operations.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const exhbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
 
 // Initializers
 const app = express();
@@ -27,8 +28,14 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(flash());
 
 // Global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 // Routes
 app.use(require('./routes/index'));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -24,7 +24,7 @@ router.post('/notes/new-note', async(req, res) => {
     } else {
         const newNote = new NoteModel({ title, description });
         await newNote.save();
-
+        req.flash('success_msg', 'Note added successfully.');
         res.redirect('/notes');
     }
 
@@ -47,8 +47,13 @@ router.get('/notes/edit/:id', async(req, res) => {
 router.put('/notes/edit-note/:id', async(req, res) => {
     const { title, description } = req.body;
     await NoteModel.findByIdAndUpdate(req.params.id, { title, description });
-    console.log(req.params.id);
-    console.log(req.body);
+    req.flash('success_msg', 'Note updated successfully')
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async(req, res) => {
+    await NoteModel.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Note deleted successfully')
     res.redirect('/notes');
 });
 
